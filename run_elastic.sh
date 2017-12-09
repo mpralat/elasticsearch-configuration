0e845c748b7a023313fb4f85a692d6e38de94a30
mpralat <pralatmarta@gmail.com>
2017-12-09 13:27:26 +0100

    Add clean flag to run_elastic

--- run_elastic.sh
+++ run_elastic.sh
@@ -11,3 +11,16 @@ cp ../jvm.options config/
 export ES_JAVA_OPTS="-Xms600m -Xmx600m"
 sysctl -w vm.max_map_count=262144
 ./bin/elasticsearch &
+
+# clean the indices if run with the clean flag
+if [ "$1" == "clean" ]
+then
+     # wait for components to run
+     printf "sleeping"
+     sleep 5s
+     printf "deleting indices..."
+     curl -XDELETE localhost:9200/count*
+     printf "setting up the mappings..."
+     cd ${SCRIPTPATH}
+     python3 setup_elastic.py
+fi
